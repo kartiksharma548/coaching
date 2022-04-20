d670e1031a11a657b4479901a1a354b79b1f725c
kartiksharma548 <kartiksharma548@gmail.com>
2022-04-20 19:22:23 +0530

    Haome Layout Added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,6 +22,28 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 2,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(10, (index) {
+          int tileNo = index + 1;
+          return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: InkWell(
+                  child: Center(
+                child: Text(
+                  'Item $tileNo',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              )));
+        }),
+      ),
+    );
   }
 }
